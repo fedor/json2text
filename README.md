8864b78f82408b93a7ce7db935ad7fddb5b63d0d
Fedor Korshunov <fedor@aftership.net>
2015-06-23 09:03:51 +0800

    0.0.1 release

---
+++ README.md
@@ -0,0 +1,70 @@
+## json2text
+
+json2text generate free-format text from JSON templates (e.g. HTML generation).
+
+Install package globally
+```
+npm install json2text -g
+```
+
+## How to use example
+
+### 1. Define template elements
+Create file with ```.j2.node.json``` extension (e.g. ```elements.j2.node.json```). Include the following text:
+```
+[
+	{
+		"_name": "list",
+		"style": "disc",
+		"_text": "<ul style='list-style-type:%%style%%'>\n%%_text%%</ul>"
+	}, {
+		"_name": "item",
+		"_text": "<li> %%_text%% </li>\n"
+	}
+]
+```
+
+JSON fields:
+ - ```_name```: node(s) names.
+ - ```_text```: node's templating string.
+ - OPTIONAL: other fileds represent arguments with default values (e.g. ```style```).
+
+### 2. Define template layout
+Create file with ```.j2.json``` extension (e.g. ```layout.j2.json```). Include the following text:
+```
+{
+	"_node":  "list",
+	"style": "circle",
+	"_child": [
+		{"_node": "item", "_child": "list item #1"},
+		{"_node": "item", "_child": "list item #2"}
+	]
+}
+```
+
+JSON fields:
+ - ```_node```: element name
+ - ```_child```: (string|object|list): node content
+ - OPTIONAL: other fileds represent arguments values (e.g. ```style = circle```).
+
+### 3. Compile template
+Run:
+```
+j2t html
+```
+
+This commend will search current work directory file-tree for ```.j2.node.json``` and ```.j2.json``` files and would compile ```.html``` output files:
+```
+<ul style='list-style-type:circle'>
+<li> list item #1 </li>
+<li> list item #2 </li>
+</ul>
+```
+
+j2t produce ```.out``` files by default.
+
+Features:
+ - TBD
+
+TODO:
+ - TBD
---
+++ index.js
@@ -0,0 +1,102 @@
+#! /usr/bin/env node
+
+var _       = require('lodash');
+var fs      = require('fs');
+var process = require('process');
+
+var files    = {};
+var elements = {};
+var basedir  = process.cwd();
+
+var r = function(p) {
+	try {
+		var j = JSON.parse(fs.readFileSync(p, {'encoding': 'utf8'}));
+		if (!_.isArray(j)) j = [j];
+		return j;
+	} catch (e) {
+		console.log(p+' not JSON!');
+		return [];
+	}
+}
+
+var get_attrs = function(e) {
+	var r = {};
+	_.each(e, function(def_value, attr) {
+		if (attr && attr[0] != '_') r[attr] = def_value;
+	});
+	return r;
+}
+
+var upd_elems = function(path) {
+	var j = r(path);
+	//console.log('>>upd elems! ' + JSON.stringify(j, null, 4));
+
+	j.forEach(function(e) {
+		try {
+			if (!_.isObject(e)) throw JSON.stringify(e)+' is invalid. skipped'
+			if (!e._name)       throw JSON.stringify(e)+': _name missed. skipped'
+			if (!e._text)       throw e._name+': _text missed. skipped'
+		
+			elements[e._name] = {
+				text: e._text,
+				attrs: get_attrs(e)
+			}
+			console.log('\t'+e._name+': loaded');
+		} catch(err) {
+			console.log('\twarn: '+err);
+		}
+	});
+
+	//console.log( 'elements: '+JSON.stringify(elements, null, 4) );
+};
+
+var use_template = function(element, node, base_payload) {
+	//console.log( 'element: '+JSON.stringify(element, null, 4) );
+	//console.log( 'node:    '+JSON.stringify(node, null, 4) );
+	//console.log( 'base:    '+JSON.stringify(base_payload, null, 4) );
+	try {
+		var payload = _.extend( _.cloneDeep(element.attrs), _.omit(node, ['_node', '_child']), {'_text': base_payload} );
+		// console.log( 'payload = '+JSON.stringify(payload, null, 4) );
+		_.templateSettings.interpolate = /%%([\s\S]+?)%%/g;
+		return _.template(element.text)(payload); // '%%<INPUT>%%');
+	} catch (err) {
+		throw node._node+': compilation failed: '+err+'. skipped'
+	}
+};
+
+var compile = function(path, parent_template) {
+	var j = path;
+	if (!_.isArray(j)) j = r(j);
+	//console.log('compile! ' + JSON.stringify(j, null, 4));
+
+	var base_parts = []
+	j.forEach(function(n) {
+		try {
+			if (!_.isObject(n))                     throw JSON.stringify(n)+' is invalid. skipped'
+			if (!n._node)                           throw JSON.stringify(n)+': node missed. skipped'
+			if (!n._child && !_.isString(n._child)) throw n._node+': child missed. skipped'
+		
+			var t = n._node;
+			var c = n._child;
+			var e = elements[t];
+			if (!e) throw t+': unknown element. skipped'
+			
+			var base_part = null;
+			if ( _.isString(c) ) base_part = c;
+			else {
+				if (!_.isArray(c)) c = [c];
+				base_part = compile(c, parent_template);
+			}
+			base_part = use_template(e, n, base_part);
+			base_parts.push(base_part);
+		} catch (err) {
+			console.log('\twarn: '+err);
+		}
+	});
+
+	return base_parts.join('');
+};
+
+var m = require('./lib/mon');
+m.mon(basedir, 'j2.node.json', null, upd_elems);
+m.mon(basedir, 'j2.json', process.argv[2] || 'out', compile);
